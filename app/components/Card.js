e384f90c4d4172f50f92f58f6540c8d35e402c15
josephlatina <josephlatina93@gmail.com>
2021-09-09 14:00:24 -0600

    laid out the placements for question and multiple choice answers

--- app/components/Card.js
+++ app/components/Card.js
@@ -5,7 +5,7 @@ function Card({ text, onPress }) {
     return (
         <TouchableWithoutFeedback onPress={onPress}>
             <View style={styles.card}>
-                <Text>{text}</Text>
+                <Text style={styles.text}>{text}</Text>
             </View>
         </TouchableWithoutFeedback>
     )
@@ -13,10 +13,23 @@ function Card({ text, onPress }) {
 
 const styles = StyleSheet.create({
     card: {
-        borderRadius: 15,
+        width: 250,
+        height: 50,
+        borderRadius: 25,
         backgroundColor: 'white',
         justifyContent: 'center',
         alignItems: 'center',
+        shadowColor: 'grey',
+        shadowOpacity: 0.5,
+        elevation: 10,
         marginBottom: 20
+    },
+    text: {
+        color: 'black',
+        fontSize: 15,
+        textTransform: 'uppercase',
+        fontWeight: '600',
     }
 })
+
+export default Card;
--- app/components/QuestionScreen.js
+++ app/components/QuestionScreen.js
@@ -1,11 +1,31 @@
 import { CardAnimationContext } from '@react-navigation/stack';
 import React from 'react';
-import { StyleSheet, ImageBackground, Image, View, FlatList } from 'react-native';
+import { StyleSheet, ImageBackground, View, FlatList, Text } from 'react-native';
 
 import AppButton from './AppButton';
+import Card from './Card';
 import Screen from './Screen';
 
-function QuestionScreen({ children, style, options }) {
+const options = [
+    {
+        id: 1,
+        text: 'Option1'
+    },
+    {
+        id: 2,
+        text: 'Option2'
+    },
+    {
+        id: 3,
+        text: 'Option3'
+    },
+    {
+        id: 4,
+        text: 'Option4'
+    },
+]
+
+function QuestionScreen({ children, style }) {
     return (
         <ImageBackground
             style={styles.background}
@@ -14,19 +34,24 @@ function QuestionScreen({ children, style, options }) {
             <View 
                 style={styles.questionBox}
             >
-                <Image 
+                <ImageBackground
                     style={styles.image}
-                    resizeMode= 'contain'
                     source={require("../assets/questionBox.png")}
                 >
-                    <FlatList 
-                        data={options}
-                        keyExtractor={option => option.id.toString()}
-                        renderItem={({ item }) =>
-                            <Card />
-                        }
-                    />
-                </Image>
+                    <Text style={styles.question}>Question 1</Text>
+                    <View style={styles.optionsContainer}>
+                        <FlatList 
+                            data={options}
+                            keyExtractor={option => option.id.toString()}
+                            renderItem={({ item }) =>
+                                <Card 
+                                    text={item.text}
+                                    onPress={() => console.log("Option Selected", item)}
+                                />
+                            }
+                        />
+                    </View>
+                </ImageBackground>
             </View>
             <View style={styles.buttonContainer}>
                 <AppButton title='Submit'/>
@@ -43,6 +68,10 @@ const styles = StyleSheet.create({
     },
     image: {
         flex: 1,
+        width: '100%',
+        height: '100%',
+        justifyContent: 'flex-end',
+        alignItems: 'center'
     },
     questionBox: {
         width: '85%',
@@ -57,7 +86,19 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top: 100,
         overflow: 'hidden'
-        
+    },
+    question: {
+        position: 'absolute',
+        top: 80,
+        fontSize: 20,
+        // textTransform: 'uppercase',
+        fontWeight: 'bold',
+    },
+    optionsContainer: {
+        padding: 20,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     buttonContainer: {
         position: 'absolute',
