5f3b513cd978f4ca78e146c569cd0e81589aed80
josephlatina <josephlatina93@gmail.com>
2021-09-09 18:26:22 -0600

    made screens for Question2 to 5 and updated the Navigator file and routes file

--- app/navigation/QuizNavigator.js
+++ app/navigation/QuizNavigator.js
@@ -2,12 +2,41 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import WelcomeScreen from '../screens/WelcomeScreen';
+import routes from './routes';
+import Question1 from '../screens/Question1';
+import Question2 from '../screens/Question2';
+import Question3 from '../screens/Question3';
+import Question4 from '../screens/Question4';
+import Question5 from '../screens/Question5';
 
 const Stack = createStackNavigator();
 
 const QuizNavigator = () => (
     <Stack.Navigator>
-        <Stack.Screen />
+        <Stack.Screen 
+            name={routes.WELCOME}
+            component={WelcomeScreen}
+        />
+        <Stack.Screen 
+            name={routes.QUESTION1}
+            component={Question1}
+        />
+        <Stack.Screen 
+            name={routes.QUESTION2}
+            component={Question2}
+        />
+        <Stack.Screen 
+            name={routes.QUESTION3}
+            component={Question3}
+        />
+        <Stack.Screen 
+            name={routes.QUESTION4}
+            component={Question4}
+        />
+        <Stack.Screen 
+            name={routes.QUESTION5}
+            component={Question5}
+        />
     </Stack.Navigator>
 )
 
---
+++ app/screens/Question4.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import QuestionScreen from '../components/QuestionScreen';
+
+const options = [
+    {
+        id: 1,
+        text: 'Option1'
+    },
+    {
+        id: 2,
+        text: 'Option2'
+    },
+    {
+        id: 3,
+        text: 'Option3'
+    },
+    {
+        id: 4,
+        text: 'Option4'
+    },
+]
+
+function Question4() {
+
+return (
+    <QuestionScreen 
+        options = {options}
+    />
+)
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default Question4;
