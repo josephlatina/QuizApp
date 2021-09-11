e28ab85341cfe4960d147948b924c6562757f4c2
josephlatina <josephlatina93@gmail.com>
2021-09-10 19:00:24 -0600

    updated lottie view animation for any button selected in the quiz

--- app/screens/Question1.js
+++ app/screens/Question1.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import LottieView from 'lottie-react-native';
 
 import QuestionScreen from '../components/QuestionScreen';
 import routes from '../navigation/routes';
@@ -29,6 +30,17 @@ function Question1({navigation}) {
     const handlePress = (id) => {
         if (id === 2) 
             setResult(result+1)
+        return (
+            <LottieView 
+            style = {{height: '100%', width: '100%', backgroundColor: 'white'}}
+            autoPlay = {false}
+            resizeMode = 'cover'
+            flexGrow = {1}
+            loop = {false}
+            source={require('../assets/animations/quiz-bump.json')}
+            onAnimationFinish = {navigation.navigate(routes.QUESTION2, result)}
+            />
+        )
     }
 
     return (
@@ -37,7 +49,7 @@ function Question1({navigation}) {
             title = 'Question 1'
             subtitle = 'When was the first release of the Apple Watch?'
             onPress={() => navigation.navigate(routes.QUESTION2, result)}
-            optionOnPress={handlePress}
+            optionOnPress={handlePress} //use callback function to pass in the id number of the button selected from child component to parent component
         />
     )
     }
